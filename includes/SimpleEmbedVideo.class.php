9dd607a07562802d183f5c0d4eb8ce9f901cb472
Solene <solene@dokit.io>
2017-10-24 16:59:11 +0200

    Suppression des derniers espaces

--- includes/SimpleEmbedVideo.class.php
+++ includes/SimpleEmbedVideo.class.php
@@ -13,10 +13,10 @@ class SimpleEmbedVideo {
 		$out = '<div class="formLinkVideo">'."\n";
 		$out .= '  <span class="embedVideoLogos embedVideoYoutube" data-servicename="youtube" data-placeholder="Entrez une vidéo Youtube" > Youtube </span>';
 		$out .= '  <span class="embedVideoLogos embedVideoVimeo" data-servicename="vimeo" data-placeholder="Entrez une vidéo Vimeo" > Vimeo </span>';
-    	$out .= '  <span class="embedVideoLogos embedVideoDailymotion" data-servicename="dailymotion" data-placeholder="Entrez une vidéo Daylimotion"> Daylimotion </span>';
+		$out .= '  <span class="embedVideoLogos embedVideoDailymotion" data-servicename="dailymotion" data-placeholder="Entrez une vidéo Daylimotion"> Daylimotion </span>';
       
-    	$out .= '  <div class="embedVideoForm">'."\n";
-    	$out .= $param1;
+		$out .= '  <div class="embedVideoForm">'."\n";
+		$out .= $param1;
 		$out .= '  </div>'."\n";
       
 		$out .= '  <div class="embedVideoFieldLink">'."\n";
@@ -25,8 +25,7 @@ class SimpleEmbedVideo {
       
 		$out .='</div>'."\n";
       
-      
-      	return array( $out, 'noparse' => true, 'isHTML' => true );
+		return array( $out, 'noparse' => true, 'isHTML' => true );
 	}
    
 	public static function renderSimpleEmbedVideo () {
