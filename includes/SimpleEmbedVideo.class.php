548846fd6b47dfa0973ebab887d1f794e3c30407
Solene <solene.moins@gmail.com>
2018-01-19 09:08:30 +0100

    Mise en place de la traduction pour les placeholder

--- includes/SimpleEmbedVideo.class.php
+++ includes/SimpleEmbedVideo.class.php
@@ -7,31 +7,31 @@ class SimpleEmbedVideo {
 		$parser->setFunctionHook( 'formLinkVideo', 'SimpleEmbedVideo::renderFormLinkVideo' );
 	}
 
-	// 
+	//
 	public static function renderFormLinkVideo( $parser, $param1 = '', $param2 = '') {
 
 		$out = '<div class="formLinkVideo">'."\n";
-		$out .= '  <span class="embedVideoLogos embedVideoYoutube" data-servicename="youtube" data-placeholder="Entrez une vidéo Youtube" > Youtube </span>';
-		$out .= '  <span class="embedVideoLogos embedVideoVimeo" data-servicename="vimeo" data-placeholder="Entrez une vidéo Vimeo" > Vimeo </span>';
-		$out .= '  <span class="embedVideoLogos embedVideoDailymotion" data-servicename="dailymotion" data-placeholder="Entrez une vidéo Daylimotion"> Daylimotion </span>';
-      
+		$out .= '  <span class="embedVideoLogos embedVideoYoutube" data-servicename="youtube" data-placeholder="'.wfMessage( "simple-embed-video-youtube" )->parse().'" > Youtube </span>';
+		$out .= '  <span class="embedVideoLogos embedVideoVimeo" data-servicename="vimeo" data-placeholder="'.wfMessage( 'simple-embed-video-vimeo' )->parse().'" > Vimeo </span>';
+		$out .= '  <span class="embedVideoLogos embedVideoDailymotion" data-servicename="dailymotion" data-placeholder="'.wfMessage( 'simple-embed-video-dailymotion' )->parse().'"> Daylimotion </span>';
+
 		$out .= '  <div class="embedVideoForm">'."\n";
 		$out .= $param1;
 		$out .= '  </div>'."\n";
-      
+
 		$out .= '  <div class="embedVideoFieldLink">'."\n";
 		$out .= $param2;
 		$out .= '  </div>'."\n";
-      
+
 		$out .='</div>'."\n";
-      
+
 		return array( $out, 'noparse' => true, 'isHTML' => true );
 	}
-   
+
 	public static function renderSimpleEmbedVideo () {
-   	
+
 	}
-   
+
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 		$out->addModules('ext.simpleembedvideo.js');
 		$out->addModuleStyles('ext.simpleembedvideo.css');
