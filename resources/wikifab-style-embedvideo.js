42a47751b4599e394573b97a2ea39767aac8b69a
Pierre Boutet <boutetpierre@yahoo.fr>
2018-12-07 11:41:45 +0100

    fix bug 972 show add button after remove video

--- resources/wikifab-style-embedvideo.js
+++ resources/wikifab-style-embedvideo.js
@@ -1,22 +1,26 @@
-		
+
 (function() {
 	// Déclarations des variables
 	var videoForm;
 	var ValeurCochee;
 	var dropdown;
-	
+
 	$(document).ready(function() {
 
 		videoForm = $('.embedVideoLogos'); // Variable qui contient les logos
 		dropdown = $('.embedVideoDropdown'); // Variable de la dropdown cachée
-		
-				
+
+
 		$('.embedVideoUrlInput').hide();
 		dropdown.hide();
 		var instanceMax = $('.multipleTemplateList').attr('maximuminstances');
 		var nbInstance = document.querySelectorAll('.multipleTemplateList .multipleTemplateInstance').length - 1;
 		if (nbInstance >= instanceMax){
 			$('.multipleTemplateHideButton .multipleTemplateAdder').hide();
+
+			$('.removeButton').click(function(){
+				$('p').children('.multipleTemplateAdder').show();
+			});
 		}
 		$('.multipleTemplateHideButton .multipleTemplateAdder').click(function(){
 			var instanceMax = $('.multipleTemplateList').attr('maximuminstances');
@@ -25,23 +29,23 @@
 				$(this).hide();
 			}
 		});
-		
-		
+
+
 		function addNewVideoForm (div) {
 			$('.removeButton').click(function(){
 				$('p').children('.multipleTemplateAdder').show();
 			});
-			
+
 			if ($(div).find('.embedVideoUrlInput').val() != ''){
 				var serviceNameSelected = $(div).find('.embedVideoDropdown').val();
-//				$(div).parents('.multipleTemplateHideButton').find('.multipleTemplateAdder').hide();	
+//				$(div).parents('.multipleTemplateHideButton').find('.multipleTemplateAdder').hide();
 				$(div).find('.embedVideoUrlInput').show();
 				$(div).find(".embedVideoLogos[data-servicename='" + serviceNameSelected + "']").addClass('active');
 			}
-			else { 
+			else {
 				$(div).find('.embedVideoUrlInput').hide();
 			}
-			// Quand on clique sur l'un des logos, la valeur du dropdown est modifiée   
+			// Quand on clique sur l'un des logos, la valeur du dropdown est modifiée
 			$(div).find('.embedVideoLogos').click(function(){
 				$(div).find('.embedVideoLogos').removeClass('active');
 				$(this).addClass('active');
@@ -49,24 +53,24 @@
 				$(div).find('.embedVideoUrlInput').show();
 				$(div).find('.embedVideoUrlInput').attr("placeholder", $(this).attr("data-placeholder"));
 				//Lorsque l'on change de service la valeur du champ est remise à 0
-				$(div).find('.embedVideoUrlInput').val(''); 
-			});	
-			
-		}		
-		
-		
-		
-		// Hook qui permet d'appeler le JS au moment où les div sont ajoutées au HTML 
+				$(div).find('.embedVideoUrlInput').val('');
+			});
+
+		}
+
+
+
+		// Hook qui permet d'appeler le JS au moment où les div sont ajoutées au HTML
 		mw.hook( 'pf.addTemplateInstance' ).add( function(div) {
-			
+
 			addNewVideoForm($(div).find('.formLinkVideo'));
-			
+
 		});
-		
+
 		$('.formLinkVideo').each(function (){
 				addNewVideoForm(this);
 		});
 
 	});
-		
+
 })();
